11687108038d322555fd9dc133d30317feb2cce4
bachduy25102004 <bachduy25102004@gmail.com>
2024-07-28 18:39:23 +0700

    notes add

--- brih.js
+++ brih.js
@@ -1,1 +1,9 @@
 console.log('blah blah')
+//      git checkout -b <branch-name>
+//      git log --graph
+//      git status
+//      git switch -
+//      git merge <branch-name>
+//      git push
+//      git pull
+//      git clone
